218538b3c9602bc0eb7277847919e0f4a6169fbf
anirudhjl <anibhat022@gmail.com>
2024-02-16 18:41:27 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const root = React.createElement('div', {},
+// const heading1 = React.createElement('div', {}, 'I am heading') // returns JS object
+
+const heading2 = React.createElement('div', {},
   React.createElement('div', { id: 'parent1' },
-    React.createElement('h1', { id: 'child1' }, 'Child 1'),
-    React.createElement('h2', { id: 'child2' }, 'Child 2')
+    React.createElement('h1', { id: 'child1' }, 'I am Child 1'),
+    React.createElement('h2', { id: 'child2' }, 'I am Child too')
   ),
   React.createElement('div', { id: 'parent2' },
-    React.createElement('h1', { id: 'child1' }, 'Child 1'),
-    React.createElement('h2', { id: 'child2' }, 'Child 2')
+    React.createElement('h1', { id: 'child1' }, 'I am Child 1'),
+    React.createElement('h2', { id: 'child2' }, 'I am Child too')
   )
 )
 
-// Render root component
-const rootContainer = ReactDOM.createRoot(document.getElementById('root'))
-rootContainer.render(root)
+const HeadingComponent = () => <h1>I am different</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render([heading2, <HeadingComponent />])
---
+++ README.md
@@ -0,0 +1,1 @@
+# Namaste React project by Akshay Saini
